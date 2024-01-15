ea8c5a2843a018daf76a7c65cb1f090752f7e3d0
aleanrocha <alean.oficial.me@gmail.com>
2024-01-15 11:57:56 -0300

    add routes

---
+++ src/containers/Movies/index.jsx
@@ -0,0 +1,9 @@
+const Movies = () => {
+  return (
+    <>
+      <h1>Filmes</h1>
+    </>
+  )
+}
+
+export default Movies
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Home from './containers/Home'
+import Movies from './containers/Movies'
+import Series from './containers/Series'
+import Root from './router/root'
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'filmes',
+        element: <Movies />
+      },
+      {
+        path: 'series',
+        element: <Series />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
 )

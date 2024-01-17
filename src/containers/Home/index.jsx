8594457c0d1efaf9825063d06071d852a3255def
aleanrocha <alean.oficial.me@gmail.com>
2024-01-17 10:39:11 -0300

    homepage structure and style

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react'
+
 import api from '../../services/api'
+import { Background, AlignContent, Info, Poster } from './styles'
+
 const Home = () => {
-  const handleGetMovies = async () => {
-    const response = await api.get('movie/popular')
-    console.log(response.data.results)
-  }
-  handleGetMovies()
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    const handleGetMovies = async () => {
+      const {
+        data: { results }
+      } = await api.get('movie/popular')
+      setMovie(results[0])
+    }
+    handleGetMovies()
+  }, [])
+
   return (
     <>
-      <h1>Home</h1>
+      {movie && (
+        <Background
+          $image={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        >
+          <AlignContent>
+            <Info>
+              <h1>{movie.original_title}</h1>
+              <p>{movie.overview}</p>
+            </Info>
+            <Poster>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={`imagem ${movie.original_title}`}
+              />
+            </Poster>
+          </AlignContent>
+        </Background>
+      )}
     </>
   )
 }

7009c914b1b8923fd91b4788a1d5239fdc1d5176
aleanrocha <alean.oficial.me@gmail.com>
2024-01-19 17:46:41 -0300

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,36 @@
+import { useState } from 'react'
+
 import logo from '../../assets/dev-movies-logo.png'
 import {
   Header as HeaderContainer,
   AlignContent,
   Img,
+  NavBarContainer,
   NavBar,
-  Link
+  Link,
+  ShowMenu,
+  CloseMenu
 } from './styles'
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleToggleMenu = () => setOpen(!open)
+
+  console.log(open)
   return (
     <HeaderContainer>
       <AlignContent>
         <Img src={logo} alt="logo dev movies" />
-        <NavBar>
-          <Link>Início</Link>
-          <Link>Filmes</Link>
-          <Link>Séries</Link>
-        </NavBar>
+        <NavBarContainer $isMobile={open}>
+          <ShowMenu $isMobile={open} onClick={handleToggleMenu} />
+          <CloseMenu $isMobile={open} onClick={handleToggleMenu} />
+          <NavBar $isMobile={open}>
+            <Link>Início</Link>
+            <Link>Filmes</Link>
+            <Link>Séries</Link>
+          </NavBar>
+        </NavBarContainer>
       </AlignContent>
     </HeaderContainer>
   )
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,3 +1,5 @@
+import { FaBars, FaX } from 'react-icons/fa6'
+
 import styled from 'styled-components'
 
 export const Header = styled.header`
@@ -6,6 +8,7 @@ export const Header = styled.header`
   height: 96px;
   display: flex;
   justify-content: center;
+  padding: 1rem 2rem;
   position: fixed;
   width: 100%;
   box-shadow: 0px 0px 5px #d7a33d;
@@ -20,14 +23,43 @@ export const AlignContent = styled.div`
   gap: 1rem;
 `
 export const Img = styled.img`
-  width: 200px;
+  width: 150px;
 `
-
+export const NavBarContainer = styled.div`
+  ${(props) =>
+    props.$isMobile
+      ? `
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    transition: 0.5s;
+    `
+      : `left: -100%;`}
+  & > svg {
+    font-size: 1.5rem;
+    cursor: pointer;
+    z-index: 5;
+  }
+`
 export const NavBar = styled.nav`
   display: flex;
   gap: 2rem;
+  ${(props) =>
+    props.$isMobile &&
+    `
+    background-color: #000000d3;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    backdrop-filter: blur(1rem);`}
+  @media screen and (max-width: 768px) {
+    display: ${(props) => (props.$isMobile ? 'flex' : 'none')};
+  }
 `
-
 export const Link = styled.a`
   color: #fafafa;
   font-weight: 700;
@@ -38,4 +70,16 @@ export const Link = styled.a`
   &:hover {
     color: #d7a33d;
   }
+`
+export const ShowMenu = styled(FaBars)`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: ${(props) => (props.$isMobile ? 'none' : 'block')};
+  }
+`
+export const CloseMenu = styled(FaX)`
+  display: ${(props) => (props.$isMobile ? 'block' : 'none')};
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
 `
